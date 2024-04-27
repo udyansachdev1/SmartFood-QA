0e1d08331fe5ea928da342a87f9b3b16685bbd6e
Revanth Chowdary <revanth7667@gmail.com>
2024-04-28 01:23:56 +0530

    deled folder

--- llm/crates/ggml/src/format/mod.rs
+++
@@ -1,7 +0,0 @@
-//! Loading and saving of [GGML](https://github.com/ggerganov/ggml) files.
-
-mod loader;
-mod saver;
-
-pub use loader::*;
-pub use saver::*;
--- llm/crates/ggml/sys/UPDATING.md
+++
@@ -1,3 +0,0 @@
-# Updating
-
-The bindings are automatically generated. See [CONTRIBUTING](../../../CONTRIBUTING.md#regenerating-the-ggml-bindings).
